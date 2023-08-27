c86cadc8e5a2f2c5a318bd2ffb5a237544778391
Melody <143276576+Melodywangxy@users.noreply.github.com>
2023-08-27 18:12:14 +0200

    Update etl.py

--- etl.py
+++ etl.py
@@ -4,18 +4,24 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """This function is to copy S3 data into staging tables on redshift"""
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """This function is to insert data from staging tables into analytic tables"""
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """This function is to establish connection to redshift database, call
+    functions to load staging tables from S3 and copy staged data into 
+    analytic tables. Close connection in the end.
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
